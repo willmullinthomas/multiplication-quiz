341131b3e1044f3274f1b22657c6182fd63ff1e7
will thomas <willmt80@gmail.com>
2023-09-05 22:40:50 -0400

    seperate db into new dev branch

--- src/view/view_helpers.py
+++ src/view/view_helpers.py
@@ -1,7 +1,6 @@
-from src.ui.model.classes.answer import Answer
-from src.ui.model.helpers import create_times_table, get_equation_list
-from src.ui.model.classes.game_state import GameState
-# from src.model.classes.answer import Answer
+from src.model.classes.answer import Answer
+from src.model.helpers import create_times_table, get_equation_list
+from src.model.classes.game_state import GameState
 
 def get_new_game(max: int, count: int):
     times_table = create_times_table(max)
